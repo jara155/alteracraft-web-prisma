3667cb202ed39224991efeccdf271b6f26ec99eb
Jája <74739998+jara155@users.noreply.github.com>
2023-08-19 00:39:26 +0200

    Delete docs directory

--- docs/_app/immutable/nodes/2.4b83457a.js
+++
@@ -1,1 +0,0 @@
-import{s}from"../chunks/scheduler.e4c1e8fb.js";import{S as t,i as e}from"../chunks/index.ac0d1271.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
